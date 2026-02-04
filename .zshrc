62c920d6b743ae9c31119fb6696c477f002a5e5a
debsource <debsource@proton.me>
2026-02-03 16:01:01 -0800

    Initial commit

--- .zshrc
+++
@@ -1,3 +0,0 @@
-eval "$(starship init zsh)"'
-clear; python /home/$USER/lufetch.py
-alias ls='ls -l --color=auto'
--- nord/rofi/launcher.sh
+++
@@ -1,8 +0,0 @@
-
-dir="$HOME/.config/rofi/styles"
-theme='style-1'
-
-## Run
-rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
